133a269959d4a3ca13a272ffa264271fb81e7182
jiewang <18723151512@163.com>
2021-08-18 08:10:59 +0800

    提交

---
+++ boot02-admin/src/main/java/com/kyel/actuator/health/MyHealthHealthIndicator.java
@@ -0,0 +1,15 @@
+package com.kyel.actuator.health;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+
+public class MyHealthHealthIndicator extends AbstractHealthIndicator {
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+
+    }
+
+
+
+
+    }
---
+++ boot02-admin/src/test/java/com/kyel/Junit5Test.java
@@ -0,0 +1,14 @@
+package com.kyel;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author jiewang
+ */
+public class Junit5Test {
+
+    @Test
+    public void test1(){
+        System.out.println("测试Junit5");
+    }
+}
