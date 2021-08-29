4f14ca851b0252e144c0aef0811cb4ffbd9d987b
jiewang <18723151512@163.com>
2021-08-29 11:33:16 +0800

    提交代码

---
+++ boot02-admin/src/main/java/com/kyel/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.kyel.controller;
+
+import configuration.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        String hello = helloService.sayHello();
+        return hello;
+    }
+}
